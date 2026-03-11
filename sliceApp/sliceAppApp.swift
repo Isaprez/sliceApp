7c260fc59d6b0e99352d9d8135ee82f5f6f3bd95
Isaprez <isai.perez@apsys.mx>
2026-03-10 20:18:51 -0600

    Initial Commit

---
+++ sliceApp/sliceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  sliceAppApp.swift
+//  sliceApp
+//
+//  Created by Isai Pérez Rojas on 10/03/26.
+//
+
+import SwiftUI
+
+@main
+struct sliceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
